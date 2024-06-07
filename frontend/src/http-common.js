f6868e6e695ee04562089db05b1c9679c951c0ee
tofaade18 <tofa.ade@bithealth.co.id>
2024-06-07 10:43:12 +0700

    deployment

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "http://54.157.51.139:3000/api",
   headers: {
     "Content-type": "application/json"
   }
